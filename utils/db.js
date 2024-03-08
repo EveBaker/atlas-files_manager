261e764aa3335b86bf0ad2e4de5381f05de7a898
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-03-08 20:49:51 +0000

    working on task 1

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,34 @@
-class DBClient{ 
+const { MongoClient } = require('mongodb');
+
+class DBClient {
     constructor() {
-        this.host
-        this.port
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || '27017';
+        const database = process.env.DB_DATABASE || 'files_manager';
+
+        this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
+        this.db = null;
+
+        this.client.connect()
+            .then(() => {
+                this.db = this.client.db(database);
+        })
+        .catch(err => console.error ('DB connection err', err));
     }
-}
-this.client
+
+    isAlive() {
+        return !!this.db;
+    }
+
+    async nbUsers() {
+        if (!this.isAlive()) return 0;
+        return this.db.collection('users').countDocuments();
+    }
+    
+    async nbFiles() {
+        if (!this.isAlive()) return 0;
+        return this.db.collection('files').countDocuments()
+     }
+    }
+    const dbClient = new DBClient();
+    module.exports = dbClient;
