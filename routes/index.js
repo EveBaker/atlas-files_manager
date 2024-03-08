d52d9633af22c09a3bdf2af12dbeb1d1aac1e684
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-03-08 23:19:26 +0000

    working on task 3:new user

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,18 @@
  * contains all endpoints of our API:
  * GET /status => AppController.getStatus
  * GET /stats => AppController.getStats
+ * POST /users => UsersController.postNew
  */
 
 const express = require('express');
 
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
