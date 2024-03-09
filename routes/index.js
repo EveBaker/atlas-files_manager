1b4800496f394c72051821baddf9f259f58b4c00
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-03-08 22:15:55 -0600

    delete duplicate line

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ const express = require('express');
 
 const router = express.Router();
 
-const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 
 
